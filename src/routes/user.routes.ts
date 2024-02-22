62ad21f75e6d21d1659bebd40d0408cda507f393
Andra Wicaksono <andra.wicaksono@bullionecosystem.com>
2024-02-22 07:26:26 +0700

    fix: add authenticate jwt

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { UserRepository } from '../repositories/user.repository';
 import { UserService } from '../services/user.service';
 import { UserController } from '../controllers/user.controller';
+import { authenticateJWT } from '../middlewares/auth.middleware';
 
 const userRouter = Router();
 
@@ -9,15 +10,19 @@ const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 const userController = new UserController(userService);
 
+
 userRouter.get('/',
+  authenticateJWT,
   userController.getUserProfile
 );
 
 userRouter.put('/',
+  authenticateJWT,
   userController.updateUserProfile
 );
 
 userRouter.delete('/',
+  authenticateJWT,
   userController.deleteUserProfile
 );
 
